f55bbfb154ca2dfede4d8cad43b2c07dfd32e542
Zolton <gmzesq@gmail.com>
2019-08-21 14:43:49 -0400

    Migration tables and seeds set up, work

---
+++ data/seeds/01-userDummyData.js
@@ -0,0 +1,28 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("users")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("users").insert([
+        { username: "admin", password: "admin", department: "sales" },
+        { username: "test1", password: "test1", department: "sales" },
+        { username: "testTweo", password: "testTweop", department: "sales" },
+        {
+          username: "superadmin",
+          password: "superadmin",
+          department: "finance"
+        },
+        {
+          username: "superadmin212",
+          password: "superadmin44",
+          department: "finance"
+        },
+        {
+          username: "testagain",
+          password: "no imagination",
+          department: "finance"
+        }
+      ]);
+    });
+};
