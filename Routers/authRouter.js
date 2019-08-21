820fa220ef4baf2859a7aa7220e4e61693b1b3f3
Zolton <gmzesq@gmail.com>
2019-08-21 15:50:37 -0400

    Registration works

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -1,6 +1,6 @@
-const router = require("express").Router()
-const bcrypt = require("bcrypt")
-const User = require("./routerFunctions")
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("./routerFunctions");
 
 // Base URL - /api/restricted
 
@@ -8,16 +8,25 @@ const User = require("./routerFunctions")
 //     res.status(200).json({Hello: "from authRouter"})
 // })
 
-router.get("/", (req, res)=>{
-User.getAll()
-.then(post=>{
-res.status(200).json(post)
-})
-.catch(error=>{
-res.status(500).json({Error: 'Server status: 500'})
-})
-}
-)
+router.get("/", (req, res) => {
+  User.getAll()
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res.status(500).json({ Error: "Server status: 500" });
+    });
+});
 
+router.post("/register", (req, res) => {
+  const user = req.body;
+  User.addNew(user)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({ Error: "Server status: 500" });
+    });
+});
 
-module.exports = router
+module.exports = router;
--- Routers/routerFunctions.js
+++ Routers/routerFunctions.js
@@ -1,9 +1,19 @@
 const db = require("../data/db-Config");
 
 module.exports = {
-  getAll
+  getAll, addNew
 };
 
 function getAll() {
   return db("users");
 }
+
+function addNew (user) {
+    return db("users").insert(user)
+}
+
+// function generateToken(user) {
+//     const payload = {
+//         subject: 
+//     }
+// }
